723ef2ba4c602369977df1164850d6a6bc6732a6
root <root@ff84a66b4923>
2022-01-05 03:56:11 +0000

    rps

--- src/_01_random/_2_rock_paper_scissors/RockPaperScissors.java
+++ src/_01_random/_2_rock_paper_scissors/RockPaperScissors.java
@@ -6,6 +6,7 @@ package _01_random._2_rock_paper_scissors;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -83,6 +84,8 @@ public class RockPaperScissors extends JPanel implements ActionListener{
         int opponentSelection = 0;
         
         //3. Run the program again. Is the result different?
+        Random rand = new Random();
+        opponentSelection = rand.nextInt(3);
  
         int selection = 0;
        
