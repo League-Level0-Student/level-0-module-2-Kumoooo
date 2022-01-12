4ce2be730f775a99fb9ee3944ed7b2b0415621e0
student <student@ip-172-31-63-196.us-west-2.compute.internal>
2022-01-11 19:57:23 -0800

    dice/val

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -12,12 +12,26 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+for (int i = 0; i < 10; i++) {
 
+	int randomNumber = randomMaker.nextInt(5);
+	
+	if (randomNumber == 0) {
+		System.out.println("You Rock");
+	} else if (randomNumber == 1) {
+		System.out.println("You're Cool!");
+	} else if (randomNumber == 2) {
+		System.out.println("You're Nice!");
+	} else if (randomNumber == 3) {
+		System.out.println("You're Funny");
+	} else if (randomNumber == 4) {
+		System.out.println("You're Amazing");
+	} 
+	
+
+}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
